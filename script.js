a344435d9cfa18d04771ea45bc3b206f91309970
Popeashiedu <38441893+Popeashiedu@users.noreply.github.com>
2020-03-24 21:27:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var name = "Benedict Ashiedu";
+var courses = ["Design","HTML","CSS","Java"];
+var numberOfCourses = courses.length;
+
+console.log(name)
+console.log(courses)
+
+for (let i=0; i<=200; i+=2){
+
+    if(i % 2===0){
+        console.log(i)
+    }
+    
+}
